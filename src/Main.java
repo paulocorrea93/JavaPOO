0ae5814b752ff1dca185c5363178d730264b8008
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-25 09:23:23 -0400

    estrutura base de classe em POO

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.nome = "paulo";
+        pessoa1.idade = 32;
+
+        pessoa1.mensagem();
+    }
+
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    public void mensagem(){
+        System.out.println("olá " + nome + " você tem " + idade + " anos.");
+    }
+
+}
