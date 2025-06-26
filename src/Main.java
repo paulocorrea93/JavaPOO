feb42a5c73e4afd1216de3027f29acfb4c6c5fce
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-26 08:05:16 -0400

    construtores

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,50 @@
 public class Main {
 
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa1 = new Pessoa("Paulooo", 332);
         Livros livro1 = new Livros();
 
-        pessoa1.nome = "paulo";
-        pessoa1.idade = 32;
-        livro1.titulo = "Lord of the Rings";
-        livro1.autor = "J.R.R Tolkien";
-
+//        livro1.titulo = "Lord of the Rings";
+//        livro1.autor = "J.R.R Tolkien";
+//
         pessoa1.mensagem();
-        livro1.infoLivro();
-
-        Calculadora calc = new Calculadora();
-
-        int soma = calc.somar(4, 9);
-        System.out.println(soma);
-
-        calc.mostrarMensagem("Gabriel");
-
-        boolean ehPar = calc.ehPar(3);
-        boolean ehPar2 = calc.ehPar(2);
-
-        System.out.println(ehPar2);
-        System.out.println(ehPar);
-
-        Funcionario pg = new Funcionario();
-        pg.nome = "Paulo Gabriel";
-        pg.salario = 2500.00;
-
-        pg.aplicarAumento(10);
-
-        pg.mostrarInfosFuncionario();
-
-        Carro creta = new Carro();
-
-        creta.marca = "creta";
-        creta.velocidade = 60.0;
-
-        creta.mostrarStatus();
-
-        creta.acelerar(40.0);
-
-        creta.mostrarStatus();
-
-        creta.frear(20);
-
-        creta.mostrarStatus();
+//        livro1.infoLivro();
+//
+//        Calculadora calc = new Calculadora();
+//
+//        int soma = calc.somar(4, 9);
+//        System.out.println(soma);
+//
+//        calc.mostrarMensagem("Gabriel");
+//
+//        boolean ehPar = calc.ehPar(3);
+//        boolean ehPar2 = calc.ehPar(2);
+//
+//        System.out.println(ehPar2);
+//        System.out.println(ehPar);
+//
+//        Funcionario pg = new Funcionario();
+//        pg.nome = "Paulo Gabriel";
+//        pg.salario = 2500.00;
+//
+//        pg.aplicarAumento(10);
+//
+//        pg.mostrarInfosFuncionario();
+//
+//        Carro creta = new Carro();
+//
+//        creta.marca = "creta";
+//        creta.velocidade = 60.0;
+//
+//        creta.mostrarStatus();
+//
+//        creta.acelerar(40.0);
+//
+//        creta.mostrarStatus();
+//
+//        creta.frear(20);
+//
+//        creta.mostrarStatus();
 
     }
 
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -3,6 +3,11 @@ public class Pessoa {
     String nome;
     int idade;
 
+    Pessoa(String n, int i){
+        nome = n;
+        idade = i;
+    }
+
     public void mensagem(){
         System.out.println("olá " + nome + " você tem " + idade + " anos.");
     }
