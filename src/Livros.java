573b3b624578a8c930dd3ec7e96d97a0851331b1
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-26 07:33:10 -0400

    metodos base

---
+++ src/Livros.java
@@ -0,0 +1,11 @@
+public class Livros {
+
+    String autor;
+    String titulo;
+
+    public void infoLivro(){
+        System.out.println("Autor: " + autor);
+        System.out.println("Titulo: " + titulo);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa();
+        Livros livro1 = new Livros();
 
         pessoa1.nome = "paulo";
         pessoa1.idade = 32;
+        livro1.titulo = "Lord of the Rings";
+        livro1.autor = "J.R.R Tolkien";
 
         pessoa1.mensagem();
+        livro1.infoLivro();
+
     }
 
 }
